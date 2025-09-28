ad4c517ba08b3de2bbc12ec6c268b50f99ffcbc3
Vinh Le <vinhpq.dt.468@gmail.com>
2025-09-28 10:34:47 +0700

    Initial commit

---
+++ src/main/java/swd/fpt/exegroupingmanagement/ExeGroupingManagementApplication.java
@@ -0,0 +1,13 @@
+package swd.fpt.exegroupingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeGroupingManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExeGroupingManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/swd/fpt/exegroupingmanagement/ExeGroupingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package swd.fpt.exegroupingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExeGroupingManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
