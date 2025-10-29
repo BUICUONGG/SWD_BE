aea06e18159819dee5c55fd7aa724f43329effc3
Vinh Le <vinhpq.dt.468@gmail.com>
2025-10-29 21:03:02 +0700

    [UPDATE] Replace Lettuce with Jedis for Redis connection and remove SSL configuration

--- src/main/java/swd/fpt/exegroupingmanagement/config/RedisConfig.java
+++ src/main/java/swd/fpt/exegroupingmanagement/config/RedisConfig.java
@@ -1,20 +1,13 @@
 package swd.fpt.exegroupingmanagement.config;
 
-
-import java.time.Duration;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import io.lettuce.core.ClientOptions;
-import io.lettuce.core.SslOptions;
 
 @Configuration
 public class RedisConfig {
@@ -28,51 +21,24 @@ public class RedisConfig {
     @Value("${spring.data.redis.password}")
     private String password;
 
-    @Value("${spring.data.redis.ssl.enabled:false}")
-    private boolean useSsl;
-
     @Bean
-    LettuceConnectionFactory lettuceConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setPort(port);
-        redisStandaloneConfiguration.setPassword(password);
-
-        LettuceClientConfiguration.LettuceClientConfigurationBuilder builder = 
-            LettuceClientConfiguration.builder();
+    JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        config.setPassword(password);
         
-        builder.commandTimeout(Duration.ofSeconds(10));
-        
-        if (useSsl) {
-            // Configure SSL for Railway Redis - disable hostname verification
-            SslOptions sslOptions = SslOptions.builder()
-                .jdkSslProvider()
-                .build();
-            
-            ClientOptions clientOptions = ClientOptions.builder()
-                .sslOptions(sslOptions)
-                .build();
-            
-            builder.clientOptions(clientOptions)
-                   .useSsl()
-                   .disablePeerVerification();  // Important for Railway Redis
-        }
-
-        LettuceClientConfiguration clientConfig = builder.build();
-
-        return new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig);
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
     RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-
-        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-
-        return redisTemplate;
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
     }
 }
