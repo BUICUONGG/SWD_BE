d404fe9d495a11d90edf70e7cfdb4c0fb74ecd4e
Vinh Le <vinhpq.dt.468@gmail.com>
2025-10-29 15:55:09 +0700

    [UPDATE] Add SSL configuration for Redis connection in application profiles and RedisConfig

--- src/main/java/swd/fpt/exegroupingmanagement/config/RedisConfig.java
+++ src/main/java/swd/fpt/exegroupingmanagement/config/RedisConfig.java
@@ -1,14 +1,19 @@
 package swd.fpt.exegroupingmanagement.config;
 
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 public class RedisConfig {
@@ -22,13 +27,37 @@ public class RedisConfig {
     @Value("${spring.data.redis.password}")
     private String password;
 
+    @Value("${spring.data.redis.ssl.enabled:false}")
+    private boolean useSsl;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(host);
         redisStandaloneConfiguration.setPort(port);
         redisStandaloneConfiguration.setPassword(password);
-        return new JedisConnectionFactory(redisStandaloneConfiguration);
+
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(10);
+        poolConfig.setMaxIdle(5);
+        poolConfig.setMinIdle(1);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        poolConfig.setTestWhileIdle(true);
+
+        JedisClientConfiguration.JedisClientConfigurationBuilder builder = 
+            JedisClientConfiguration.builder();
+        builder.connectTimeout(Duration.ofSeconds(60));
+        builder.readTimeout(Duration.ofSeconds(60));
+        builder.usePooling().poolConfig(poolConfig);
+
+        if (useSsl) {
+            builder.useSsl();
+        }
+
+        JedisClientConfiguration clientConfig = builder.build();
+
+        return new JedisConnectionFactory(redisStandaloneConfiguration, clientConfig);
     }
 
     @Bean
